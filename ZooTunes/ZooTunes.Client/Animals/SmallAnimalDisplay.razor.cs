7ca95ccc89cbf085de47c13871807c54264e15b5
Jean-Luc Jordi <jean-luc.jordi@digitecgalaxus.ch>
2025-01-23 22:21:08 +0100

    fix unfavorite on favorite page

--- ZooTunes/ZooTunes.Client/Animals/SmallAnimalDisplay.razor.cs
+++ ZooTunes/ZooTunes.Client/Animals/SmallAnimalDisplay.razor.cs
@@ -7,13 +7,22 @@ namespace ZooTunes.Client.Animals;
 public partial class SmallAnimalDisplay : ComponentBase
 {
     [Parameter] [EditorRequired] public required AnimalData AnimalData { get; set; }
+    [Parameter] public EventCallback OnStateChanged { get; set; }
+
+    private void ChangeState()
+    {
+        // Change some state
+        // Notify the parent
+        StateHasChanged();
+        OnStateChanged.InvokeAsync();
+    }
 
     private void ToggleFavorite()
     {
         AnimalData.IsFavorite = !AnimalData.IsFavorite;
         var result = AnimalData.IsFavorite ? "The animal is now a favorite" : "The animal is not a favorite anymore";
-        snackbar.Add(result, Severity.Success);     
-        StateHasChanged();
+        snackbar.Add(result, Severity.Success);
+        ChangeState();
     }
 
     public async Task OpenAnimalDescription()
